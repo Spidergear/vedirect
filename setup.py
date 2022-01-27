40b07447108bd716b67b0940c09b5b74ce880ff4
Spidergear <34096834+Spidergear@users.noreply.github.com>
2022-01-27 16:43:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='vedirect',
       version='0.1',
       description='Victron VE.Direct decoder for Python',
-      url='https://github.com/karioja/vedirect',
-      author='Janne Kario',
-      author_email='janne.kario@gmail.com',
+      url='https://github.com/Spidergear/vedirect',
+      author='Dieter Lubbe',
+      author_email='DieterLubbe@gmail.com',
       license='MIT',
       packages=['vedirect'],
       install_requires=[
